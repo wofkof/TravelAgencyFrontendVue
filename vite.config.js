3bcb5fa2bb1649242ec8593729fa7a8ddd7337c5
wofkof <qwex0088@gmail.com>
2025-04-23 10:18:10 +0800

    EditFramework

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
-// https://vite.dev/config/
+import Components from "unplugin-vue-components/vite";
+import AutoImport from "unplugin-auto-import/vite";
+
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    Components({
+      dirs: ["src/components"],
+      extensions: ["vue"],
+      deep: true,
+    }),
+    AutoImport({
+      imports: ["vue", "vue-router"],
+      dts: "src/auto-imports.d.ts",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:7265",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/styles/global.scss";`,
+      },
+    },
+  },
+});
