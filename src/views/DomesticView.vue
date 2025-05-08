9507570d2fb62edead73ec0dbfd04c2db813772c
Maggy <yonezawaaini@gmail.com>
2025-05-08 14:31:07 +0800

    修正點擊連結跳到頁尾的錯誤

--- src/views/DomesticView.vue
+++ src/views/DomesticView.vue
@@ -16,10 +16,10 @@
     <div class="bg-white rounded-xl p-4 text-center text-gray-700 shadow">
       搜尋結果(未元件化)
       <router-link to="/Trip">
-        <div class="bg-blue-100 rounded-lg p-4 mb-4">
-        內容 01
-        </div>
-      </router-link>
+      <div class="bg-blue-100 rounded-lg p-4 mb-4">
+      內容 01
+      </div>
+    </router-link>
         
 
         <div class="bg-blue-100 rounded-lg p-4 mb-4">
