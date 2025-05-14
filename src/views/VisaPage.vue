467c7ab03943e315503e03e589d5b3e9d7031d25
MINGW64 <b7468204@gmail.com>
2025-05-14 23:52:44 +0800

    製作header和footer

--- src/views/VisaPage.vue
+++ src/views/VisaPage.vue
@@ -1,12 +1,32 @@
 <!-- Views/VisaPage.vue -->
 <template>
-   <el-carousel :interval="4000" type="card" height="300px">
-    <el-carousel-item v-for="(img, index) in images" :key="index">
-      <img :src="img" class="carousel-image" />
-    </el-carousel-item>
-  </el-carousel>
+    <!-- 上面的輪播圖 -->
+  <div class="common-layout">
+    <el-container>
+      <el-header style="height: auto;">
+        <div style="margin-bottom: 20px">
+          <el-carousel :interval="4000" type="card" height="300px">
+            <el-carousel-item v-for="(img, index) in images" :key="index">
+              <img :src="img" class="carousel-image" />
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-header>
 
-  <VisaTabs /><!-- 這是 tab 元件 -->
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>Main Content Here</el-main>
+
+          <el-footer>
+            <VisaInfoFooter/>
+          </el-footer>
+          
+        </el-container>
+      </el-container>
+    </el-container>
+
+  </div>
 </template>
 
 
@@ -19,10 +39,11 @@ const images = [
    new URL('../assets/images/visa5.jpg', import.meta.url).href,
    new URL('../assets/images/visa6.jpg', import.meta.url).href,
 ]
+import { ref } from 'vue';
+import { ArrowRight } from '@element-plus/icons-vue'; 
+import VisaInfoFooter from '@/components/Visa/VisaInfoFooter.vue'; 
 
-import VisaTabs from '@/components/Visa/VisaTabs.vue'
 </script>
-
 
 <style scoped>
 .carousel-image {
@@ -31,4 +52,5 @@ import VisaTabs from '@/components/Visa/VisaTabs.vue'
   object-fit: cover;
   border-radius: 8px;
 }
+
 </style>
