e11eb49fbe2ded8a3e4035172ce974f435589431
Maggy <yonezawaaini@gmail.com>
2025-05-21 12:20:41 +0800

    畫面綁定至日程(1-2)

--- src/views/Trip.vue
+++ src/views/Trip.vue
@@ -8,7 +8,7 @@
 
   <!-- 標題區塊 -->
   <div class="bg-yellow-100 rounded-lg p-4 mb-4 text-gray-800 font-semibold text-left text-2xl">
-    {{ detail.title }}
+    {{ mainInfo.title }}
   </div>
 
   <div class="flex h-96">
@@ -17,13 +17,15 @@
 
     <div class="w-1/2 flex gap-2 p-4 text-left relative">
       <div class="w-1/2 rounded-md">
-        <img :src="detail.cover" alt="" class="h-56 w-max" >
+        <img :src="mainInfo.cover" alt="" class="h-56 w-max" >
       </div>
       <div class="text-lg font-bold my-2">
-          <p>行程 {{detail.number}}</p>
-          <p>可賣： {{ detail.availableSeats }}  席次：{{ detail.totalSeats }}</p>
+          <p>行程 1</p>
+          <p>可賣： 1 席次：1</p>
           <p>去程：{{ formattedDepartureDate }}</p>
           <p>回程：{{ formattedReturnDate }}</p>
+          <p>{{mainInfo.country}}/{{ mainInfo.region }}</p>
+          <p>{{ mainInfo.price }}</p>
       </div>
       
       
@@ -52,14 +54,14 @@
                       </tr>
                   </thead>
                   <tbody>
-                      <tr>
-                          <td>1</td>
-                          <td>2</td>
-                          <td>3</td>
-                          <td>4</td>
-                          <td>5</td>
-                          <td>6 元</td>
-                      </tr>
+                    <tr v-for="g in groupList" :key="groupList.groupId">
+                      <td>{{ formatDateTime(g.departure, { type: 'date' })}}</td>
+                      <td>2</td>
+                      <td>{{g.availableSeats}}</td>
+                      <td>{{g.totalSeats}}</td>
+                      <td>{{g.groupStatus}}</td>
+                      <td>6 元</td>
+                    </tr>
                   </tbody>
                 
               </table>
@@ -70,7 +72,7 @@
 </div>
 
 <div class="bg-white rounded-xl p-4 shadow mb-6 text-xl font-medium">
-{{ detail.description }}
+{{ mainInfo.description }}
 
   </div>
 
@@ -78,15 +80,15 @@
 
   <div class="bg-white rounded-xl p-4 shadow mb-6">
     <div class="demo-collapse">
-        <el-collapse v-for="s in detail.schedules" :key="s.day">
+        <el-collapse >
           <el-collapse-item name="1">
             <template #title="{ isActive }">
               <div :class="['title-wrapper']" class="text-xl font-medium">
-                第{{s.day}}天
+                第天
               </div>
           </template>
             <div class="text-lg text-gray-900 leading-relaxed">
-              {{s.scheduleDescription}}
+              
             </div>
           </el-collapse-item>
     </el-collapse>
@@ -103,50 +105,41 @@
     import { onMounted, ref } from 'vue';
     import { useRoute } from 'vue-router';
     import api from '@/utils/api';
-    import axios from 'axios';
     import { formatDateTime } from '@/utils/formatDateTime';
     import normalButton from "@/components/official/normalButton.vue";
 
-    const detail = ref(
+    const mainInfo = ref(
     {
       "projectId": 0,
-      "detailId": 0,
       "title": "",
+      "departure": "",
+      "return": "",
       "description": "",
       "cover": "",
-      "number": 0,
-      "adultPrice": 0,
-      "groupTravelId": 0,
-      "departureDate": "",
-      "returnDate": "",
-      "availableSeats": 0,
-      "totalSeats": 0,
-      "schedules": [
+      "price": 0,
+      "country": "",
+      "region": ""
+    }
+  );
+    const groupList = ref(
+      [
         {
-          "scheduleId": 0,
-          "day": 0,
-          "scheduleDescription": "",
-          "breakfast": "",
-          "lunch": "",
-          "dinner": "",
-          "hotel": "",
-          "attraction1": 0,
-          "attraction2": 0,
-          "attraction3": 0,
-          "attraction4": 0,
-          "attraction5": 0
+          "groupId": 0,
+          "departure": "",
+          "return": "",
+          "totalSeats": 0,
+          "availableSeats": 0,
+          "groupStatus": ""
         },
       ]
-    },
+    );
+
+  const formattedDepartureDate = computed(() =>
+    formatDateTime(mainInfo.value.departure, { type: 'date' })
   );
-
-const formattedDepartureDate = computed(() =>
-  formatDateTime(detail.value.departureDate, { type: 'date' })
-);
-const formattedReturnDate = computed(() =>
-  formatDateTime(detail.value.returnDate, { type: 'date' })
-);
-
+  const formattedReturnDate = computed(() =>
+    formatDateTime(mainInfo.value.return, { type: 'date' })
+  );
 const route = useRoute();
 
 
@@ -154,11 +147,21 @@ onMounted(async () => {
   const projectId = route.params.projectId;
   const detailId = route.params.detailId;
   const groupId = route.params.groupId;
+  
   try {
-    const res = await api.get(`/OfficialSearch/detail/${projectId}/${detailId}/${groupId}`);
-    detail.value = Array.isArray(res.data) ? res.data[0] : res.data;
+    const res = await api.get(`/OfficialSearch/getMainInfo/${projectId}/${detailId}/${groupId}`);
+    mainInfo.value = res.data;
   } catch (err) {
-    console.error("取得行程詳情失敗", err);
+    console.error("取得行程mainInfo失敗", err);
   }
+
+  try {
+    const glist = await api.get(`/OfficialSearch/getGrouplist/${projectId}`);
+    groupList.value = glist.data;
+    console.log()
+  } catch (err) {
+    console.error("取得行程groupList失敗", err);
+  }
+
 });
   </script>
