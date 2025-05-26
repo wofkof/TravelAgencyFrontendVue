6668964a2edd394b79d74c38cee235797dc732b7
MINGW64 <b7468204@gmail.com>
2025-05-27 00:31:22 +0800

    new

--- vite.config.js
+++ vite.config.js
@@ -39,7 +39,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://localhost:7265",
-        changeOrigin: true
+        changeOrigin: true,
         secure: true, // 允許代理到使用自簽章憑證的後端
         rewrite: (path) => path.replace(/^\/api/, ""),
         // rewrite: (path) => path,
