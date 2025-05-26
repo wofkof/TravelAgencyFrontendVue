8f2e32245dc200e547ee4dbf8729fc5ca07ac414
wofkof <qwex0088@gmail.com>
2025-05-26 16:24:09 +0800

    修改首頁圖片與頁尾和刪除導覽列關於我們

--- src/components/travellian/FooterSection.vue
+++ src/components/travellian/FooterSection.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
     <div class="l-footer-wrapper">
-      <div class="logo-copyright">
+      <div class="logo-copyright" style="margin-top: -50px">
         <img
           src="@/assets/images/logo.png"
           alt="Travellian logo"
@@ -12,51 +12,51 @@
           版權所有 © 嶼你同行 2025 保留所有權利
         </div>
       </div>
-      <nav class="footer-menu">
-        <h4 class="footer-menu__hdl">Menu</h4>
-        <div class="footer-menu__item">Home</div>
-        <div class="footer-menu__item">Explore</div>
-        <div class="footer-menu__item">Travel</div>
-        <div class="footer-menu__item">Blog</div>
-        <div class="footer-menu__item">Pricing</div>
-      </nav>
-      <nav class="info">
-        <h4 class="info__hdl">Information</h4>
-        <div class="info__item">Destinations</div>
-        <div class="info__item">Supports</div>
-        <div class="info__item">Terms & Conditions</div>
-        <div class="info__item">Privacy</div>
-      </nav>
       <nav class="contact">
-        <h4 class="contact__hdl">Contact Info</h4>
-        <div class="contact__item">+123 456 789</div>
+        <h4 class="contact__hdl">聯絡方式</h4>
+        <div class="contact__item">02 6631 6588</div>
         <div class="contact__item">
-          <a href="mailto:info@travellian.com">info@travellian.com</a>
+          <a href="mailto:info@travellian.com">maylin@ispan.com.tw</a>
         </div>
-        <div class="contact__item">1245, New York, USA</div>
+        <div class="contact__item">106,台北市大安區復興南路一段390號2樓</div>
       </nav>
       <nav class="social">
-        <h4 class="social__hdl">Follow us on</h4>
-        <img
-          src="/images/fb_social.svg"
-          alt="Facebook icon"
-          class="social__img"
-        />
-        <img
-          src="/images/pinterest_social.svg"
-          alt="Pinterest icon"
-          class="social__img"
-        />
-        <img
-          src="/images/insta_social.svg"
-          alt="Instagram icon"
-          class="social__img"
-        />
-        <img
-          src="/images/twitter_social.svg"
-          alt="Twitter icon"
-          class="social__img"
-        />
+        <h4 class="social__hdl">關注我們</h4>
+        <div style="display: flex">
+          <a
+            href="https://www.facebook.com/ispanTaipei/?locale=zh_TW"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/fb_social.svg"
+              alt="Facebook icon"
+              class="social__img"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/ispan_educate/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/insta_social.svg"
+              alt="Instagram icon"
+              class="social__img"
+            />
+          </a>
+          <a
+            href="https://www.ispan.com.tw/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/twitter_social.svg"
+              alt="Twitter icon"
+              class="social__img"
+            />
+          </a>
+        </div>
       </nav>
     </div>
   </footer>
