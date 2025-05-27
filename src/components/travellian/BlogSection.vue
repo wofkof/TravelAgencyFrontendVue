a34dd2e91cc79fed5d1930162e1afa1afde03a86
ST <shanna869724@gmail.com>
2025-05-27 14:56:03 +0800

    首頁預設內容小修改

--- src/components/travellian/BlogSection.vue
+++ src/components/travellian/BlogSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="blog">
     <h2 class="blog__hdl">旅遊部落格</h2>
-    <h3 class="blog__subhdl">洞察世界上令人難以置信的體驗</h3>
+    <!-- <h3 class="blog__subhdl">洞察世界上令人難以置信的體驗</h3> -->
     <article class="blog__post post">
       <div class="post__photo">
         <img
@@ -11,12 +11,10 @@
         />
       </div>
       <div class="post__content">
-        <h2 class="post__hdl">美麗的義大利<br />一起旅行</h2>
+        <h2 class="post__hdl">魅力台灣<br />一起探索</h2>
         <p class="post__text">
-          但我必須向你們解釋，這種譴責快樂、讚揚痛苦的錯誤觀念是如何產生的，
-          我將向你們完整地介紹這個體系，並闡述偉大的真理探索者、人類幸福的總設計師的實際教誨。
-          沒有人會因為快樂本身而拒絕、厭惡或迴避快樂，而是因為理性會遭遇極度痛苦的後果。
-          也不再有人愛，也沒有人追求。
+          台灣，是山海之間的寶島，也是融合傳統與現代的精彩之地。
+          <br>從台北的城市節奏，到花蓮、台東的自然壯麗，再到台南的古城文化與夜市美食，每一站都是驚喜。<br>無論你是初次造訪，或是深入體驗，在這裡都能感受到人情味與在地魅力，等你一起出發探索！
         </p>
         <span class="post__read-more">閱讀更多</span>
       </div>
