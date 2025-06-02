0d64cbaa6a5f088d37b792da0cdb37a4224b17b3
Aeceer <b193666666@gmail.com>
2025-06-02 15:02:46 +0800

    6/2發票OK吧

--- src/router/index.js
+++ src/router/index.js
@@ -131,9 +131,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  // scrollBehavior(to, from, savedPosition) {
-  //   return { top: 0 };
-  // },
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
 });
 
 export default router;//富成
